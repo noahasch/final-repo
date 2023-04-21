edab42efa043539ece106ca2ffa9ee167125586b
Noah Asch <nasch@andrew.cmu.edu>
2023-04-21 17:25:20 +0000

    First Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pkg',
+      version='0.0.1',
+      description='final project',
+      maintainer='Noah Asch',
+      maintainer_email='nasch@andrew.cmu.edu',
+      license='MIT',
+      packages=['pkg'],
+      scripts=[],
+      long_description='''OpenAlex based package''')
